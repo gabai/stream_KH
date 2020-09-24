47dbb30130febf98ac22f05801da61f418110807
Gabriel Anaya <anaya_gabriel@outlook.com>
2020-09-24 11:42:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -808,8 +808,6 @@ p_m5 = (st.sidebar.number_input(
 "Transition to school/college reopening, people adhering to mask-wearing", 0.0, 100.0, value=55.0 ,step=5.0, format="%f")/100.0)
 p_m6 = (st.sidebar.number_input(
 "College Reopening - Face mask adherance", 0.0, 100.0, value=50.0 ,step=5.0, format="%f")/100.0)
-"College Reopening - Face mask adherance", 0.0, 100.0, value=50.0 ,step=5.0, format="%f")/100.0)
-
 
 delta_p = 1/(st.sidebar.number_input(
 "Days a person is pre-symptomatic", 0.0, 10.0, value=1.7 ,step=1.0, format="%f"))
